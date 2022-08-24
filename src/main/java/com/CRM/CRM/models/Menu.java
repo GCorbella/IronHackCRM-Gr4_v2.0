d4e8d16ce2fc9e6e21b991bcd4b7851989693be0
GCorbella <guillepcorbella@hotmail.com>
2022-08-24 19:11:41 +0200

    Arrays for commands added.

--- src/main/java/com/CRM/CRM/models/Menu.java
+++ src/main/java/com/CRM/CRM/models/Menu.java
@@ -2,5 +2,10 @@ package com.CRM.CRM.models;
 
 public class Menu {
 
-    public static void run(){}
+    private String[] commands = {"help","new lead", "show leads", "convert", "close-lost", "close-won", "new salesrep", "report", "mean", "median", "max", "min", "quit"};
+    private String[] reportCommands = {"by salesrep", "by product", "by country", "by city", "by industry"};
+    private String[] mCommands = {"employeecount", "quantity", "opps per account"};
+    private String[] sRCommands = {"leads", "opportunities", "closed-won", "closed-lost", "open"};
+    private String[] pCCICommands = {"opportunities", "closed-won", "closed-lost", "open"};
+
 }
