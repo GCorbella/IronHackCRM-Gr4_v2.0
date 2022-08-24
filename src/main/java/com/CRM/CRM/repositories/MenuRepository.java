9a3be3b526c6921ef6e63fbc83131791d3fb9ca9
MarcMarimon <crammarimon@hotmail.com>
2022-08-24 20:13:22 +0200

    Querys done

--- src/main/java/com/CRM/CRM/repositories/MenuRepository.java
+++ src/main/java/com/CRM/CRM/repositories/MenuRepository.java
@@ -1,5 +1,6 @@
 package com.CRM.CRM.repositories;
 
+import com.CRM.CRM.models.Account;
 import com.CRM.CRM.models.Hook;
 import com.CRM.CRM.models.Opportunity;
 import com.CRM.CRM.models.SalesRep;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MenuRepository extends JpaRepository {
 
@@ -69,5 +71,39 @@ public interface MenuRepository extends JpaRepository {
     @Query("SELECT * FROM opportunity WHERE status=OPEN GROUP BY industry")
     List<Object[]> findAllOpenOpportunitiesByIndustry();
 
+    @Query("SELECT AVG (employee_count) FROM account AS mean_employee_count")
+    Optional<Account> findEmployeeCountAverage();
 
+    @Query("SELECT employee_count PERCENTILE_CONT(0.5) FROM account AS median_employee_count")
+    Optional<Account> findEmployeeCountMedian();
+
+    @Query("SELECT MAX(employee_count)  FROM account AS max_employee_count")
+    Optional<Account> findEmployeeCountMax();
+
+    @Query("SELECT MIN(employee_count)  FROM account AS min_employee_count")
+    Optional<Account> findEmployeeCountMin();
+
+    @Query("SELECT AVG (product) FROM opportunity AS mean_product_quantity")
+    Optional<Opportunity> findProductQuantityAverage();
+
+    @Query("SELECT product PERCENTILE_CONT(0.5) FROM opportunity AS median_product_quantity")
+    Optional<Opportunity> findProductQuantityMedian();
+
+    @Query("SELECT MAX (product) FROM opportunity AS max_product_quantity")
+    Optional<Opportunity> findProductQuantityMax();
+
+    @Query("SELECT MIN (product) FROM opportunity AS min_product_quantity")
+    Optional<Opportunity> findProductQuantityMin();
+
+    @Query("SELECT AVG (opportunities) FROM account AS mean_opportunities_per_account")
+    Optional<Account> findOpportunitiesPerAccountAverage();
+
+    @Query("SELECT opportunities PERCENTILE_CONT(0.5) FROM account AS median_opportunities_per_account")
+    Optional<Account> findOpportunitiesPerAccountMedian();
+
+    @Query("SELECT MAX (opportunities) FROM account AS max_opportunities_per_account")
+    Optional<Account> findOpportunitiesPerAccountMax();
+
+    @Query("SELECT MIN (opportunities) FROM account AS min_opportunities_per_account")
+    Optional<Account> findOpportunitiesPerAccountMin();
 }
