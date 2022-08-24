f6f1848219f68956d5148e734bf4775d7a7560b9
GCorbella <guillepcorbella@hotmail.com>
2022-08-24 16:14:44 +0200

    Hook and Contact inheritance fixed.

--- src/main/java/com/CRM/CRM/models/Hook.java
+++ src/main/java/com/CRM/CRM/models/Hook.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Hook {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private int id;
     private String name;
     private String phoneNumber;
