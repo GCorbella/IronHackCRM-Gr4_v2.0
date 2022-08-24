8cfd65da7d5d93f00a47a97d03d233f18d1c9432
GCorbella <guillepcorbella@hotmail.com>
2022-08-24 22:11:15 +0200

    Marc merged to Guille. Garbage code from repositories deleted.

--- src/main/java/com/CRM/CRM/repositories/HookRepository.java
+++ src/main/java/com/CRM/CRM/repositories/HookRepository.java
@@ -1,12 +1,9 @@
 package com.CRM.CRM.repositories;
 
 import com.CRM.CRM.models.Hook;
-import com.CRM.CRM.models.Opportunity;
-import com.CRM.CRM.models.SalesRep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface HookRepository extends JpaRepository<Hook, Integer> {
--- src/main/java/com/CRM/CRM/repositories/SalesRepRepository.java
+++ src/main/java/com/CRM/CRM/repositories/SalesRepRepository.java
@@ -1,10 +1,7 @@
 package com.CRM.CRM.repositories;
 
-import com.CRM.CRM.models.Hook;
-import com.CRM.CRM.models.Opportunity;
 import com.CRM.CRM.models.SalesRep;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
