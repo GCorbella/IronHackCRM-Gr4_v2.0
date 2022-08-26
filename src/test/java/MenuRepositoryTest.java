2b23739b6fedead7a9669cf9d9cd6c6b9776d36b
RaulGalanDelgado <110462806+RaulGalanDelgado@users.noreply.github.com>
2022-08-26 12:47:34 +0200

    errores corregidos

--- src/test/java/MenuRepositoryTest.java
+++ src/test/java/MenuRepositoryTest.java
@@ -36,17 +36,17 @@ public class MenuRepositoryTest {
         opportunity3 = new Opportunity(Product.HYBRID, contact, Status.CLOSED_LOST, account, salesRep);
         opportunityRepository.saveAll(List.of(opportunity1, opportunity2, opportunity3));
 
-
+    }
         @Test
         @DisplayName("A count of all Opportunities by SalesRep insert a correct value")
-        public List<Objects>findOpportunitiesBySalesRepCV(){
+        public void findOpportunitiesBySalesRepCV(){
             System.setIn((InputStream) salesRep.getOpportunities()) ;
             Assertions.assertEquals(1,findOpportunitiesBySalesRepCV.size());
 
         }
         @Test
         @DisplayName("A count of all Opportunities by SalesRep insert a incorrect value")
-        public List<Objects> findOpportunitiesBySalesRepIV(){
+        public void findOpportunitiesBySalesRepIV(){
             System.setIn((InputStream) salesRep.getOpportunities()) ;
             Assertions.assertEquals(0, findOpportunitiesBySalesRepIV.size());
 
@@ -54,7 +54,7 @@ public class MenuRepositoryTest {
 
         @Test
         @DisplayName("A count of all Hook by SalesRep insert a correct value")
-        public List<Objects> findHooksBySalesRepCV(){
+        public void findHooksBySalesRepCV(){
             System.setIn((InputStream) salesRep.getHooks()) ;
             Assertions.assertEquals(1, findHooksBySalesRepCV.size());
 
@@ -62,202 +62,97 @@ public class MenuRepositoryTest {
 
         @Test
         @DisplayName("A count of all Hook by SalesRep insert a incorrect value")
-        public List<Objects> findHooksBySalesRepIV(){
+        public void  findHooksBySalesRepIV(){
             System.setIn((InputStream) salesRep.getHooks()) ;
             Assertions.assertEquals(0, findHooksBySalesRepIV.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesBySalesRepCV() {
+        @DisplayName("A count closed won opportunities by saleRep  ")
+        public void findAllClosedWonOpportunitiesBySalesRepCV() {
+            System.setIn((InputStream) Opportunities.getSalesRep());
             Assertions.assertEquals(1, findAllClosedWonOpportunitiesBySalesRep.size());
 
         }
 
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesBySalesRepIV() {
+        @DisplayName("A count closed won opportunities by saleRep ")
+        public void findAllClosedWonOpportunitiesBySalesRepIV() {
+            System.setIn((InputStream) Opportunities.getSalesRep().StatusClosedWon);
             Assertions.assertEquals(0, findAllClosedWonOpportunitiesBySalesRep.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesBySalesRepCV() {
+        @DisplayName("A count closed lost opportunities by saleRep ")
+        public void findAllClosedLostOpportunitiesBySalesRepCV() {
+            System.setIn((InputStream) opportunity1.getSalesRep().Status.CLOSED.lost);
             Assertions.assertEquals(1, findAllClosedLostOpportunitiesBySalesRep.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesBySalesRepIV() {
+        @DisplayName(" Invalid A count closed lost opportunities by saleRep ")
+        public void findAllClosedLostOpportunitiesBySalesRepIV() {
+            System.setIn((InputStream) opportunity1.getSalesRep().Status.CLOSED.lost);
             Assertions.assertEquals(0, findAllClosedLostOpportunitiesBySalesRep.size());
 
         }
         //5
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesBySalesRepCV (){
+        @DisplayName("A count open opportuniyies by saleRep  ")
+        public void findAllOpenOpportunitiesBySalesRepCV (){
+            System.setIn((InputStream) opportunity1.getSalesRep().Status.OPEN);
             Assertions.assertEquals(1, findAllOpenOpportunitiesBySalesRep.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesBySalesRepIV(){
+        @DisplayName("Invalid A count open opportuniyies by saleRep  ")
+        public void findAllOpenOpportunitiesBySalesRepIV(){
+            System.setIn((InputStream) opportunity1.getSalesRep().Status.OPEN);
             Assertions.assertEquals(0, findAllOpenOpportunitiesBySalesRep.size());
 
             //------------------------------------------------------
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByProductCV () {
+        @DisplayName("A count of all Opportunities by Product insert a correct value ")
+        public void findAllOpportunitiesByProductCV () {
+            System.setIn((InputStream) Product.getAllOpportunities()) ;
             Assertions.assertEquals(1, findAllOpportunitiesByProduct.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByProductIV () {
+        @DisplayName(" Invalid count of all Opportunities by Product insert a incorrect value ")
+        public void findAllOpportunitiesByProductIV () {
+            System.setIn((InputStream) Product.getAllOpportunities()) ;
             Assertions.assertEquals(0, findAllOpportunitiesByProduct.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByProductCV () {
+        @DisplayName("A count closed won opportunities by Product ")
+        public void findAllClosedWonOpportunitiesByProductCV () {
+            System.setIn((InputStream) Opportunities.getProduct().StatusClosedWon);
             Assertions.assertEquals(1, findAllClosedWonOpportunitiesByProduct.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByProductIV () {
+        @DisplayName("Invalid A count closed won opportunities by Product \"")
+        public void findAllClosedWonOpportunitiesByProductIV () {
+            System.setIn((InputStream) Opportunities.getProduct().StatusClosedWon);
             Assertions.assertEquals(0, findAllClosedWonOpportunitiesByProduct.size());
 
         }
         @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByProductCV () {
+        @DisplayName(" A count closed lost opportunities by Product ")
+        public void findAllClosedLostOpportunitiesByProductCV () {
+            System.setIn((InputStream) opportunity1.getProduct().Status.CLOSED.lost);
             Assertions.assertEquals(1, findAllClosedLostOpportunitiesByProduct.size());
 
         }
 
         @Test
         @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByProductIV () {
+        public void findAllClosedLostOpportunitiesByProductIV () {
+            System.setIn((InputStream) opportunity1.getProduct().Status.CLOSED.lost);
             Assertions.assertEquals(0, findAllClosedLostOpportunitiesByProduct.size());
 
         }
-
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesByProductCV () {
-            Assertions.assertEquals(1, findAllOpenOpportunitiesByProduct.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesByProductIV () {
-            Assertions.assertEquals(0, findAllOpenOpportunitiesByProduct.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByCountryCV () {
-            Assertions.assertEquals(1, findAllOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByCountryIV () {
-            Assertions.assertEquals(0, findAllOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByCountrycV () {
-            Assertions.assertEquals(1, findAllClosedWonOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByCountryIV () {
-            Assertions.assertEquals(0, findAllClosedWonOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByCountryCV () {
-            Assertions.assertEquals(1, findAllClosedLostOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByCountryIV () {
-            Assertions.assertEquals(0, findAllClosedLostOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesByCountryCV () {
-            Assertions.assertEquals(1, findAllOpenOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesByCountryIV () {
-            Assertions.assertEquals(0, findAllOpenOpportunitiesByCountry.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByCityCV() {
-            Assertions.assertEquals(1, findAllOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpportunitiesByCityIV () {
-            Assertions.assertEquals(0, findAllOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByCityCV () {
-            Assertions.assertEquals(1, findAllClosedWonOpportunitiesByCity.size());
-
-        }
-
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedWonOpportunitiesByCityIV () {
-            Assertions.assertEquals(0, findAllClosedWonOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByCityCV () {
-            Assertions.assertEquals(1, findAllClosedLostOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllClosedLostOpportunitiesByCityIV () {
-            Assertions.assertEquals(0, findAllClosedLostOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects> findAllOpenOpportunitiesByCityCV () {
-            Assertions.assertEquals(1, findAllOpenOpportunitiesByCity.size());
-
-        }
-        @Test
-        @DisplayName("A ")
-        public List<Objects>findAllOpenOpportunitiesByCityIV () {
-            Assertions.assertEquals(0, findAllOpenOpportunitiesByCity.size());
-
-        }
-
-
-
-    }
 }
-
-
-
